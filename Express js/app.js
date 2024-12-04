9dc5583205cb21ca63b9be205a960ef92c2646b2
Nahuel Amado <amado.nahuel@outlook.com>
2024-12-04 18:14:36 -0300

    reiniciando curso de Express js

--- Express js/app.js
+++ Express js/app.js
@@ -1,12 +1,37 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
-const desiredPort = process.env.PORT ?? 1234
+const port = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
-    console.log('request recived')
-    res.end('Hola Mundo')
-})
+const processRequest = (req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200 //OK
+        res.setHeader('Content-Type', 'text/plain ;charset=utf-8')
+        res.end('Welcome to Home Page')
+    } else if (req.url === '/photo') {
+        fs.readFile('./photo.jpg', (err, data) => {
+            if (err) {
+                res.statusCode = 500
+                res.end('<h1>500 Internal Error</h1>')
+            } else {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'image/jpg')
+                res.end(data)
+            }
+        })
+    } else if (req.url === '/contact') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        res.end('<h1>Contactanos</h1>')
+    } else {
+        res.statusCode = 404 //Not Found
+        res.setHeader('Content-Type', 'text/html')
+        res.end('<h1>404</h1>')
+    }
+}
 
-server.listen(desiredPort, () => {
+const server = http.createServer(processRequest)
+
+server.listen(port, () => {
     console.log(`server listening on port http://localhost:${port}`)
 })
