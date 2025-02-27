896da0b6ff2d10928bb4bbc5c472f7372d8f12d4
Nahuel Amado <amado.nahuel@outlook.com>
2025-02-26 21:57:58 -0300

    terminada la clase de API REST, CORS y Cabeceras

--- Api REST clase 3/app.js
+++ Api REST clase 3/app.js
@@ -1,69 +1,137 @@
 const express = require('express')
 const crypto = require('node:crypto')
-const movies = require('/movies.json')
+const cors = require('cors')
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemes/movies')
 
 const app = express()
 app.disable('x-powered-by')
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 app.use(express.json())
 
+
+
 app.get('/', (req, res) => {
-    res.json({ message: 'hola mundirijillo'})
+  res.json({ message: 'hola mundirijillo' })
 })
 
-app.get('movies',  (req, res)=> {
-    const { genre } = req.query
-    if (genre) {
-        const filterMovies = movies.filter(
-            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-        )
-    }
-    res.json(movies)
+app.get('/movies', (req, res) => {
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:8080') //
+
+  // const origin = req.header('origin')
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+
+  const { genre } = req.query
+  if (genre) {
+    const filterMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+
+    res.json(filterMovies)
+  }
+
+  res.json(movies)
 })
 
-app.get('/movies/:id', (req, res)=>{
-    const {id} = req.params
-    const movie = movies.find(movie => movie.id === id)
-    if (movie) return res.json(movie)
-    
-    res.status(404).json({message: 'Movie noy found'})
-})
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
 
-app.get('/movies/:id', (req, res)=>{
-    const {id} = req.params
-    const movie = movies.find(movie => movie.id === id)
-    if (movie) return res.json(movie)
-    
-    res.status(404).json({message: 'Movie noy found'})
+  res.status(404).json({ message: 'Movie noy found' })
 })
 
 app.post('/movies', (req, res) => {
-    const {
-        title,
-        genre,
-        year,
-        director,
-        duration,
-        rate,
-        poster       
-    } = req.body
+  const result = validateMovie(req.body)
 
-    const newMovie = {
-        id: crypto.randomUUID(),
-        title,
-        genre,
-        year,
-        director,
-        duration,
-        rate: rate ?? 0,
-        poster   
-    }
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
 
-    movies.push(newMovie)
-    res.status(201).json(newMovie)
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)// no seria REST por que guartda estado//
+  res.status(201).json(newMovie)
 })
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.messaje) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  // const origin = req.header('origin')
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  //   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')// Añadido
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type')// Añadido si es necesario
+  // }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) { return res.status(404).json({ message: 'Movie not found takataka' }) }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: ' Movie deleted' })
+})
+
+// app.options('/movies/:id', (req, res) => {
+//   const origin = req.header('origin')
+
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type') // Añadir esto si es necesario
+//   }
+//   res.sendStatus(200)
+// })
 
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {
-    console.log(`^server listening on port http//localhost:${PORT}`)
+  console.log(`server listening on port http//localhost:${PORT}`)
 })
